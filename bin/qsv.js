05afa0bc319a105ccb8e51062aa2a6d4dbc9e053
Pascal Spiesz <pascal@slamcode.com>
2018-11-17 19:27:04 +0100

    added bin

---
+++ bin/qsv.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs').parse()
+
+const readFile = require('./../src/readFile')
+const parseCsv = require('./../src/parseFile')
+const repl = require('./../src/repl')
+
+async function start () {
+  try {
+    if (yargs.p) {
+      const options = {
+        delimiter: yargs.d || '',
+        header: yargs.h || false
+      }
+
+      const data = await readFile(yargs.p, 'utf8')
+      const parsedData = await parseCsv(data, options)
+      repl(parsedData)
+    }
+  } catch (error) {
+    process.stderr.write(error)
+  }
+}
+
+start()
