345f43c41fc206af0193750b9e67636423684e26
Pascal Spiesz <pascal@slamcode.com>
2018-11-17 17:38:57 +0100

    Simple Select Statements working.

--- src/repl.js
+++ src/repl.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk')
 const readline = require('readline')
 const renderTable = require('./renderTable')
+const parse = require('@herrfugbaum/q')
+const _ = require('lodash')
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,14 +10,27 @@ const rl = readline.createInterface({
   prompt: chalk.cyan('QSV> ')
 })
 
+const getData = (sqlParserResult, parsedData) => {
+  if (sqlParserResult.type === 'SELECT_STMT') {
+    const columns = sqlParserResult.selectClause.columns
+
+    if (columns[0] !== '*') {
+      const selectedColumns = _.map(parsedData, (obj) => _.pick(obj, columns))
+      return selectedColumns
+    }
+    return parsedData
+  }
+}
+
 const repl = (parsedData) => {
 rl.prompt()
 
 rl.on('line', (line) => {
-  console.log(line.toUpperCase())
-  if(line === 'YO') {
-    console.log(renderTable(parsedData))
-  }
+  const sqlParserResult = parse(line)
+  console.log(sqlParserResult)
+  const processedTable = getData(sqlParserResult, parsedData)
+  process.stdout.write(renderTable(processedTable))
+
   rl.prompt()
 })
 }
