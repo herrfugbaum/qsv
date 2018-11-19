7c1f5376a08fe7063243014d9afd47329af9ef8d
Pascal Spiesz <pascal@slamcode.com>
2018-11-19 17:29:31 +0100

    colorize based on types

--- src/renderTable.js
+++ src/renderTable.js
@@ -3,12 +3,22 @@
 const table = require('table').table
 const chalk = require('chalk')
 
+const colorizeObject = obj => {
+  return Object.keys(obj).map(key => {
+    const item = obj[key]
+    switch (typeof item) {
+      case 'number':
+        return chalk.yellow(item)
+      default:
+        return chalk.green(item)
+    }
+  })
+}
+
 const renderTable = data => {
   const headers = [Object.keys(data[0]).map(key => chalk.red(key))]
 
-  const values = data.map(row => {
-    return Object.values(row)
-  })
+  const values = data.map(row => colorizeObject(row))
 
   const output = table(headers.concat(values))
   return output
