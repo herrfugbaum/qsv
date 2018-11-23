56775f909f20d542969676e7578ec31e85be59ea
Pascal <pascal@slamcode.com>
2018-11-23 13:32:46 +0100

    Feature/jsdocs (#9)
    * docs: Add docs for select
    
    * docs: Add docs for sql-processors
    
    * chore: Add jsdoc
    
    * chore: Add jsdoc config
    
    * docs: Add basic documentation
    
    * docs: Add docs

--- src/sql-processors/select.js
+++ src/sql-processors/select.js
@@ -1,5 +1,27 @@
 const _ = require('lodash')
 
+/**
+ * Exports the select function.
+ * @module sql-processors/select
+ * @requires lodash
+ */
+
+/**
+ * Executes the SQL SELECT statement on an Array of Objects.
+ * @function select
+ * @param {Array} columns - An Array of Strings containting the columns the SELECT statement should return.
+ * @param {Array} table - The Array of Objects on which the SELECT statement should be executed.
+ * @returns {Array} - An Array of Objects containing only the selected columns, or the original array if called with ['*']
+ * @example
+ * select(['*'], [{name: 'Bob', age: 55}, {name: 'Aisha', age: 34}])
+ * // returns [{name: 'Bob', age: 55}, {name: 'Aisha', age: 34}]
+ * @example
+ * select(['name, age'], [{name: 'Bob', age: 55}, {name: 'Aisha', age: 34}])
+ * // returns [{name: 'Bob', age: 55}, {name: 'Aisha', age: 34}]
+ * @example
+ * select(['age'], [{name: 'Bob', age: 55}, {name: 'Aisha', age: 34}])
+ * // returns [{age: 55}, {age: 34}]
+ */
 const select = (columns, table) => {
   if (columns[0] !== '*') {
     return _.map(table, obj => _.pick(obj, columns))
--- src/sql-processors/sql-processors.js
+++ src/sql-processors/sql-processors.js
@@ -3,6 +3,19 @@ const where = require('./where')
 const orderBy = require('./orderBy')
 const limit = require('./limit')
 
+/**
+ * Exports the sql-processor functions under it's namespace
+ * @module sql-processors
+ * @requires select
+ * @requires where
+ * @requires orderBy
+ * @requires limit
+ * @see module:sql-processors/select
+ * @see module:sql-processors/where
+ * @see module:sql-processors/orderBy
+ * @see module:sql-processors/limit
+ */
+
 module.exports = {
   select,
   where,
