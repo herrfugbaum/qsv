81923ede24a259a08f357b515c1d4b2b3165bf76
Pascal Spiesz <pascal@slamcode.com>
2020-04-25 20:27:26 +0200

    trying statemachines

--- src/repl.js
+++ src/repl.js
@@ -2,32 +2,143 @@
 
 const chalk = require('chalk')
 const readline = require('readline')
+const { Machine, interpret, assign } = require('xstate')
 const renderTable = require('./renderTable')
 const parse = require('@herrfugbaum/q')
 
 const executeSql = require('./excuteSql')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: chalk.cyan('QSV> '),
+const pageLength = 1
+
+const replMachine = Machine(
+  {
+    id: 'replMachine',
+    initial: 'repl',
+    context: {
+      cursor: 0,
+      data: [],
+    },
+    states: {
+      repl: {
+        on: {
+          ENTER_KEY: 'parsing',
+        },
+      },
+      parsing: {
+        on: {
+          SUCCESS: {
+            target: 'readMore',
+            actions: ['dataAssign'],
+          },
+          ERROR: 'error',
+        },
+      },
+      readMore: {
+        on: {
+          SPACE_KEY: [
+            {
+              target: 'readMore',
+              cond: 'notOnLastPage',
+              actions: ['cursorIncrement'],
+            },
+            {
+              target: 'repl',
+              actions: ['cursorReset', 'dataReset'],
+            },
+          ],
+          readAll: {
+            target: 'repl',
+            actions: ['cursorReset'],
+          },
+        },
+      },
+      error: {
+        on: {
+          '': {
+            target: 'repl',
+            actions: ['printError'],
+          },
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      cursorIncrement: assign({
+        cursor: context => context.cursor + pageLength,
+      }),
+      cursorReset: assign({
+        cursor: context => (context.cursor = 0),
+      }),
+      dataAssign: assign({ data: (context, event) => event.data }),
+      dataReset: context => (context.data = []),
+      printError: () => console.log('Some Error occured!'),
+    },
+    guards: {
+      notOnLastPage: (context, event) => context.cursor < context.data.length,
+    },
+  },
+)
+
+const replService = interpret(replMachine).onTransition(state => {
+  console.log(state.value)
+  console.log(
+    `State: ${state.value}
+Cursor: ${state.context.cursor},
+DataLength: ${state.context.data.length}`,
+  )
 })
 
+replService.start()
+
+readline.emitKeypressEvents(process.stdin)
+process.stdin.setRawMode(true)
+
 const repl = parsedData => {
-  rl.prompt()
+  replService.onTransition(state => {
+    if (state.value === 'repl') {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: chalk.cyan('QSV> '),
+      })
+      rl.removeAllListeners()
 
-  rl.on('line', line => {
-    try {
-      const sqlParserResult = parse(line)
-      const processedTable = executeSql(sqlParserResult, parsedData)
-
-      process.stdout.write(renderTable(processedTable))
-      process.stdout.write('\n')
-    } catch (error) {
-      process.stdout.write(error.message + '\n')
+      rl.prompt()
+      rl.on('line', line => {
+        replService.send('ENTER_KEY')
+        const sqlParserResult = parse(line)
+        const processedTable = executeSql(sqlParserResult, parsedData)
+        replService.send('SUCCESS', { data: processedTable })
+        const data = replService.state.context.data
+        const cursor = replService.state.context.cursor
+        const page = data.slice(cursor, cursor + pageLength)
+        process.stdout.write(renderTable(page))
+        process.stdout.write('\n')
+      })
     }
-    rl.prompt()
   })
 }
+
+process.stdin.on('keypress', (str, key) => {
+  if (replService.state.value !== 'readMore') return false
+
+  if (key.ctrl && key.name === 'c') {
+    process.exit()
+  }
+
+  if (key.name === 'space') {
+    replService.send('SPACE_KEY')
+
+    const data = replService.state.context.data
+    const cursor = replService.state.context.cursor
+    if (cursor < data.length) {
+      const page = data.slice(cursor, cursor + pageLength)
+      process.stdout.write('\n')
+      process.stdout.write(renderTable(page))
+      process.stdout.write('\n')
+    }
+  }
+})
 
 module.exports = repl
